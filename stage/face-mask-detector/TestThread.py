5698e5337fa35d414654a020c58ee09bad7bac49
root <lai29u@etu.univ-lorraine.fr>
2020-07-10 18:13:35 +0200

    update

--- stage/face-mask-detector/TestThread.py
+++ stage/face-mask-detector/TestThread.py
@@ -5,6 +5,7 @@ import sys
 import almath
 import motion
 import random
+# -*- encoding: UTF-8 -*-
 
 class Fios(Thread):
     def __init__(self,val):
@@ -16,7 +17,7 @@ class Fios(Thread):
 	self.weightsPath = os.path.sep.join(["face_detector","res10_300x300_ssd_iter_140000.caffemodel"])
 	self.faceNet = cv2.dnn.readNet(self.prototxtPath, self.weightsPath)
 	self.label = ""
-
+	self.arrive=False
 	# load the face mask detector model from disk
 	print("[INFO] loading face mask detector model...")
 	self.maskNet = load_model("mask_detector.model")
@@ -71,7 +72,8 @@ class Fios(Thread):
     def parole(self):
 	nbHuman = 0
 	nbMasque = 0
-	while True:
+
+	while self.arrive ==False:
 		self.pepper.tracker_service.registerTarget("Face", 0.15)
 		self.pepper.tracker_service.setMode("Move")
 		self.pepper.tracker_service.track("Face")
@@ -100,6 +102,8 @@ class Fios(Thread):
 			n = random.randint(0,100)
 		if self.label=="No Mask":
 			self.pepper.blink_eyes([255, 0, 0])
+			self.pepper.tablet_service.showImage("https://png.pngtree.com/png-clipart/20200401/original/pngtree-hand-drawn-2019-new-corona-virus-wearing-a-mask-figure-png-image_5329325.jpg")
+			#self.pepper.tablet_service.hideImage()
 			nbHuman = nbHuman+1
 			if m < 2:
 				self.pepper.say("Portez votre masque si vous plait.")
@@ -116,6 +120,7 @@ class Fios(Thread):
 			print"I've met "+str(nbHuman)+" peoples,there're "+str(nbMasque)+" masks"
 			time.sleep(2)
 		elif self.label=="Mask":
+			
 			self.pepper.blink_eyes([0, 255, 0])
 			nbHuman = nbHuman+1
 			nbMasque = nbMasque+1
@@ -326,7 +331,7 @@ class Fios(Thread):
 
         self.home = self.pepper.navigation_service.getRobotPositionInMap()
         print "saved home position:"+str(self.home[0])
-	tours = 4
+	tours = 1
 	while tours > 0:
 		print "go to point a : "+str(0)+","+str(0)
 		self.pepper.navigation_service.navigateToInMap([0.,0.,0.0])
@@ -367,35 +372,85 @@ class Fios(Thread):
 		print "reached home:"+str(self.pepper.navigation_service.getRobotPositionInMap()[0])
 		tours = tours - 1
         self.pepper.posture_service.goToPosture("StandInit",0.5)
+	self.arrive = True
+	print "arrive="+str(self.arrive)
+    def setDialog(self):
+	    self.pepper.dialog_service.setLanguage("French")
+#75
+	    # writing topics' qichat code as text strings (end-of-line characters are important!)
+	    topic_content_1 = ('topic: ~example_topic_content()\n'
+		               'language: frf\n'
+		               'concept:(aliments) [fruits poulet oeufs boeuf]\n'
+		               'proposal: veuillez me parler\n'
+		               'u: (Bonjour) Bonjour humain.\n'
+		               'u: (Je [veux "voudrais"] {quelques} _~aliments) Daccord! Vous devez vraiment aimer les $1 .\n'
+		               'u: (comment vas tu aujourd hui) Je vais bien merci, et toi?\n'
+		               'u: (ca va?) Super!\n'
+		               'u: (Pepper) Oui. Voua avez des questions?\n'
+		               'u: (Jai deja porter le masque) Bien!\n'
+		               'u: (quest-ce que tu mange?) Je suis un robot, je nai pas besoin de manger les aliments. Jai besoin de seulement electricite.\n'
+		               'u: (As-tu bien dormi?) Non! Tu as oublie de me couper et me charger!\n'
+		               'u: (posez-moi une question) aimez-vous le poisson? u1: (oui) ce est bon pour la sante u1: (non) je prefere la viande.\n'
+		               'u: (parlez des animaux) avez-vous un chat ou un chien? u1: (jai un chat) super u1: (jai un chien) je prefere un chat u1: (non) ah. dommage.\n'
+		               'u: (mon prenom est _*) Bonjour $name=$1\n'
+		               'u: (Quel est mon prenom?) ^first["votre prenom est $name" "je ne sais pas"] u1:(oui) rara u1: (non) OK ^clear(name)\n'
+		               'u: (Quelle est votre position?) ma position est ^break ^call(ALRobotPosture.getPosture()) c1:(_*) $1\n'
+		               'u: ([e:FrontTactilTouched e:MiddleTactilTouched e:RearTactilTouched]) Tu as touche ma tette\n'
+		               'u: ([e:faceDetected "Salut"]) Salut humain!\n')
+
+	    topic_content_2 = ('topic: ~dummy_topic()\n'
+		               'language: frf\n'
+		               'u:(test) [a b "c d" "e f g"]\n')
+
+	    # # Loading the topics directly as text strings
+	    topic_name_1 = self.pepper.dialog_service.loadTopicContent(topic_content_1)
+	    topic_name_2 = self.pepper.dialog_service.loadTopicContent(topic_content_2)
+
+	    # # Activating the loaded topics
+	    self.pepper.dialog_service.activateTopic(topic_name_1)
+	    self.pepper.dialog_service.activateTopic(topic_name_2)
+
+	    # # Starting the dialog engine - we need to type an arbitrary string as the identifier
+	    # # We subscribe only ONCE, regardless of the number of topics we have activated
+	    self.pepper.dialog_service.subscribe('my_dialog_example')
 
     def run(self):
 	if self.val == 1:
+		# Ensure that the tablet wifi is enable
+		self.pepper.tablet_service.enableWifi()
+		self.setDialog()
 		key = ""
-		#self.pepper.autonomous_life_off()
-		self.pepper.motion_service.wakeUp()
+	#self.pepper.autonomous_life_off()
+	#self.pepper.motion_service.wakeUp()
 		#self.def_pointSuivant()
 		#self.pepper.explore(4.0)
 		#while True:
 		#	if key == ord("q"):
 		#		break
-		#self.pepper.posture_service.goToPosture("StandInit",0.5)				
+		self.pepper.posture_service.goToPosture("StandInit",0.5)				
 		#self.pepper.remoteTerminal()
-		#self.navigateToPoint()		
-		#self.steps()
-		self.videoStream()
+		try:
+			self.navigateToPoint()		
+			#self.steps()
+		#self.videoStream()
+			raw_input("\nSpeak to the robot using rules from both the activated topics. Press Enter when finished:")
+		finally:
+			print 'ok'
+			# stopping the dialog engine
+			self.pepper.dialog_service.unsubscribe('my_dialog_example')
+
+			# Deactivating all topics
+			self.pepper.dialog_service.deactivateTopic(topic_name_1)
+			self.pepper.dialog_service.deactivateTopic(topic_name_2)
+
+			# now that the dialog engine is stopped and there are no more activated topics,
+			# we can unload all topics and free the associated memory
+			self.pepper.dialog_service.unloadTopic(topic_name_1)
+			self.pepper.dialog_service.unloadTopic(topic_name_2)
 		#self.def_point_show()
 		#self.steps_show()
 	if self.val == 2:
-		try:
-			self.parole()
-		except KeyboardInterrupt:
-			print "KeyBoard Interrupt initiated"
-			self.pepper.motion_service.stopMove()
-			exit()
-		except Exception, errorMsg:
-		        print str(errorMsg)
-		        print "This example is not allowed on this robot."
-		        exit()	
+		self.parole()
 
 myThread1 = Fios(1)
 myThread1.setName('Thread 1')
